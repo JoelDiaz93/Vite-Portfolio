d9106cd037b784610ae1248dc23040cad1a65314
JoelDiaz93 <cdiaz_jl@hotmail.com.ar>
2024-08-22 19:40:52 -0500

    Fix responsive design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import AnimatedText from "./AnimatedText";
 import EarthCanvas from "./canvas/Earth";
 
 const Hero = () => {
   const [replay, setReplay] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   const placeholderText = [
     {
@@ -30,12 +52,10 @@ const Hero = () => {
 
   return (
     <section className="relative w-full h-screen">
-      <div
-        className="absolute inset-0 top-[120px] lg:max-w-7xl mx-auto flex flex-row items-center gap-5"
-      >
+      <div className="absolute inset-0 top-[90px] lg:max-w-7xl mx-auto flex flex-row items-center gap-5">
         <div className="container w-full mx-auto">
           <div className="flex flex-wrap items-center">
-            <div className="flex-1 p-4 h-60 w-[300px] lg:h-80">
+            <div className="flex-1 p-1 h-40 w-[300px] lg:h-80">
               <EarthCanvas />
             </div>
             <div className="flex-2 lg:flex-1 w-full p-8 sm:my-10">
@@ -45,10 +65,7 @@ const Hero = () => {
                 // animate="visible"
                 animate={replay ? "visible" : "hidden"}
               >
-                <div
-                  className="flex flex-col"
-                  onClick={handleReplay}
-                >
+                <div className="flex flex-col" onClick={handleReplay}>
                   {placeholderText.map((item, index) => {
                     return <AnimatedText {...item} key={index} />;
                   })}
@@ -61,17 +78,17 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-8 bottom-12 w-full flex justify-center items-center">
         <a href="">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-[#D7DEDC] flex justify-center items-start p-2">
+          <div className="w-[28px] h-[50px] md:w-[35px] md:h-[64px] rounded-3xl border-2 md:border-4 border-[#D7DEDC] flex justify-center items-start p-2">
             <motion.div
               animate={{
-                y: [0, 24, 0],
+                y: isMobile ? [0, 10, 0] : [0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-[#D7DEDC] mb-1"
+              className="w-1 h-1 md:w-3 md:h-3 rounded-full bg-[#D7DEDC]"
             />
           </div>
         </a>
